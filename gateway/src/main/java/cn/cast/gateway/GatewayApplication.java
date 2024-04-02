acc42b2d2a952239feef684738667bf8fa734091
JIAO <jiaoyongshun720@gmail.com>
2024-04-02 17:06:30 +0200

    Gate网关快速入门

---
+++ gateway/src/main/java/cn/cast/gateway/GatewayApplication.java
@@ -0,0 +1,15 @@
+package cn.cast.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @autor: 我亦无他，唯手熟尔
+ */
+
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
